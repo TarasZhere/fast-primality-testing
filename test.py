2f9c929954f6cbd48361c2c7b4717698656dcddd
taras_zhere <35704223+TarasZhere@users.noreply.github.com>
2023-04-30 16:49:16 -0400

    inti

--- test.py
+++ test.py
@@ -9,8 +9,6 @@ if __name__=="__main__":
         557940830126698960967415391, # Not prime
         10106665597294733930808268834911, # Not prime - divisibile by 3
         2305567963945518424753102147331756071, # Not prime
-
-        # Found this number to test which is one of the largest prime numbers 
         170141183460469231731687303715884105727, # This is a prime
         201181192685185252019112523, # not prime
         # The number bellow is a prime
